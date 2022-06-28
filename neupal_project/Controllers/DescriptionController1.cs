eb395bd55a45dac5bf34d46c80a4f50fa1c360d6
Neupal <neupal22@gmail.com>
2022-06-28 08:39:05 +0545

    first commit

---
+++ neupal_project/Controllers/DescriptionController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace neupal_project.Controllers
+{
+    public class DescriptionController1 : Controller
+    {
+        public ViewResult Samsung()
+        {
+            return View();
+        }
+    }
+}
